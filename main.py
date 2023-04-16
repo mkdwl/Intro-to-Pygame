2a5bd8382807f8d3ecf816aaeb616fe1f3bcdf72
Mohammed Kadiwal <86614419+mkdwl@users.noreply.github.com>
2023-04-16 07:19:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import pygame
+from sys import exit
+
+pygame.init()
+screen= pygame.display.set_mode((800,400))
+
+sky_surface= pygame.image.load('graphics\Sky.png').convert()
+ground_surface = pygame.image.load('graphics\ground.png').convert()
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+    pygame.display.update()
+
